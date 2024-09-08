ead76c239795a67375963057dc8cfd8374713094
Hetvi <shahhetvi578@gmail.com>
2024-09-08 22:22:49 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  FaSquareXTwitter,
+  FaSquareInstagram,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa6";
+
+const Footer = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  return (
+    <>
+      <footer>
+        <div>
+          <img src="/logo.png" alt="logo" />
+        </div>
+        <div>
+          <h4>Support</h4>
+          <ul>
+            <li>Jobify @LimeRoad , SindhuBhavan</li>
+            <li>jobzee14@gmail.com</li>
+            <li>+91 9508904078</li>
+          </ul>
+        </div>
+
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li to={"/"}>
+              <Link>Home</Link>
+            </li>
+            <li to={"/jobs"}>
+              <Link>Jobs</Link>
+            </li>
+            {isAuthenticated && (
+              <li>
+                <Link to={"/dashboard"}>Dashboard</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+        <div>
+          <h4>Follow Us</h4>
+          <ul>
+            <li>
+              <Link to={"https://x.com/?lang=en-in"}>
+                <span>
+                  <FaSquareXTwitter />
+                </span>
+                <span>Twitter (X)</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"https://www.instagram.com/"}>
+                <span>
+                  <FaSquareInstagram />
+                </span>
+                <span>Instagram</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"https://www.youtube.com/"}>
+                <span>
+                  <FaYoutube />
+                </span>
+                <span>Youtube</span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"https://www.linkedin.com/login"}>
+                <span>
+                  <FaLinkedin />
+                </span>
+                <span>LinkedIn</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <div className="copyright">
+        &copy; CopyRight 2024. All Rights Reserved By Jobify
+      </div>
+    </>
+  );
+};
+
+export default Footer;
